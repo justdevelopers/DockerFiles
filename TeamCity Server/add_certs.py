d0182ed30a890b053ac25b4e6a8220adf7f5162d
justdevelopers <justdevelopers@ya.ru>
2015-06-10 11:24:04 +0300

    Update add_certs.py

--- TeamCity Server/add_certs.py
+++ TeamCity Server/add_certs.py
@@ -5,30 +5,26 @@ import subprocess
 
 class CertificateImporter:
 
-    def __init__(self, storage_directory, storage_password):
+    def __init__(self, storage_path, storage_password):
 
-        self.storage_directory = storage_directory
+        self.storage_path = storage_path
         self.storage_password = storage_password
-
-        if not os.path.exists(self.storage_directory):
-
-            os.makedirs(storage_directory)
 
     def list(self, location_directory):
 
         certificates = list()
 
+        if not os.path.isdir(location_directory):
+
+                return certificates
+
         for filename in os.listdir(location_directory):
 
             file_path = os.path.join(location_directory, filename)
 
-            if not os.path.isdir(file_path):
-
-                return certificates
-
             extension = os.path.splitext(filename)[1][1:].strip()
 
-            if extension == ".crt":
+            if extension == "crt":
 
                 certificates.append(file_path)
 
@@ -36,19 +32,15 @@ class CertificateImporter:
 
     def install(self, certificate_path):
 
-        filename = os.path.split(certificate_path)[:-1]
-        new_path = os.path.join(storage_directory, filename)
-        os.rename(certificate_path, new_path)
+        subprocess.call(["keytool", "-importcert", "-noprompt", "-trustcacerts", "-file", certificate_path, "-keystore", self.storage_path, "-storepass", self.storage_password])
 
-        subprocess.call(["keytool", "-importcert", "-noprompt", "-trustcacerts", "-file", new_path, "-keystore", self.storage_directory, "-storepass", self.storage_password])
-       
 if __name__ == "__main__":
 
-    storage_directory = os.environ.get("STORAGE_DIRECTORY")
-    storage_password = os.environ.get("STORAGE_PASSWORD")
+    storage_path = os.environ.get("STORAGE_PATH")
     mount_directory = os.environ.get("MOUNT_DIRECTORY")
+    storage_password = os.environ.get("STORAGE_PASSWORD")
 
-    importer = CertificateImporter(storage_directory, storage_password)
+    importer = CertificateImporter(storage_directory)
 
     for certificate_path in importer.list(mount_directory):
 
